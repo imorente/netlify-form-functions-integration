4096c56e26a5c82f8eb6935af2401a753f989219
Irene Morente <irene@netlify.com>
2019-07-29 22:17:14 -0700

    Extract send mail function

--- functions/send-email.js
+++ functions/send-email.js
@@ -2,6 +2,9 @@ const fetch = require("node-fetch");
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
 
+/* Verifies submission has been invoked by a Netlify webhook
+https://www.netlify.com/docs/webhooks/#url-notifications
+*/
 function signed(event) {
   const signature = event.headers["x-webhook-signature"];
   if (!signature) {
@@ -9,45 +12,16 @@ function signed(event) {
     return false;
   }
 
-  const { sha256 } = jwt.verify(signature, process.env.JWS_SECRET);
+  const { iss, sha256 } = jwt.verify(signature, process.env.JWS_SECRET);
   const hash = crypto
     .createHash("sha256")
     .update(event.body)
     .digest("hex");
-  console.log("hash: " + hash + ". sha:" + sha256);
-  return sha256 === hash;
+
+  return iss === "netlify" && sha256 === hash;
 }
 
-exports.handler = async (event, context) => {
-  // Only allow POST
-  if (event.httpMethod !== "POST") {
-    return { statusCode: 405, body: "Method Not Allowed" };
-  }
-
-  console.log("Event: " + JSON.stringify(event));
-  console.log("Context: " + JSON.stringify(context));
-
-  if (!signed(event)) {
-    console.log("Invalid signature");
-    return {
-      statusCode: 403
-    };
-  }
-
-  const { name, email, data = {} } = JSON.parse(event.body);
-
-  if (!email) {
-    console.log("Missing email address");
-    return {
-      statusCode: 422,
-      body: "Missing email"
-    };
-  }
-
-  console.log(
-    `Sending email to ${name} <${email}>. Data: ` + JSON.stringify(data)
-  );
-
+function sendEmail({ name, email }) {
   return fetch("https://mandrillapp.com/api/1.0/messages/send.json", {
     headers: {
       "content-type": "application/json",
@@ -65,21 +39,52 @@ exports.handler = async (event, context) => {
             type: "to"
           }
         ],
-        autotext: "true",
-        subject: "test",
-        html: "test"
+        subject: "This is the subject of the email",
+        text: "This is the body of the email"
       }
     })
-  })
+  });
+}
+
+exports.handler = async (event, context) => {
+  if (event.httpMethod !== "POST") {
+    return {
+      statusCode: 405,
+      body: "Method Not Allowed"
+    };
+  }
+
+  if (!signed(event)) {
+    return {
+      statusCode: 403,
+      body: "Invalid signature"
+    };
+  }
+
+  const { name, email, data = {} } = JSON.parse(event.body);
+
+  if (!email) {
+    console.log("Missing email address");
+    return {
+      statusCode: 422,
+      body: "Missing email"
+    };
+  }
+
+  console.log(
+    `Sending email to ${name} <${email}>. Data: ` + JSON.stringify(data)
+  );
+
+  return sendEmail({ name, email })
     .then(() => {
-      console.log("email sent");
+      console.log(`Email sent to ${name} <${email}>`);
       return {
         statusCode: 200,
         body: "Email sent"
       };
     })
     .catch(error => {
-      console.log("Something went wrong: " + error);
+      console.log(`Error sending email to ${name} <${email}>: ${error}`);
       return {
         statusCode: 422,
         body: `Oops! Something went wrong. ${error}`
