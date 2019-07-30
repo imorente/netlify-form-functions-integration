deb82ac12d2486dd84cee71b688d25adc089fe5f
Irene Morente <irene@netlify.com>
2019-07-29 18:17:51 -0700

    Change function to slack example

--- functions/test.js
+++ functions/test.js
@@ -1,4 +1,5 @@
 const querystring = require("querystring");
+const fetch = require("node-fetch");
 
 exports.handler = async (event, context) => {
   // Only allow POST
@@ -7,12 +8,24 @@ exports.handler = async (event, context) => {
   }
 
   // When the method is POST, the name will no longer be in the event’s
-  // queryStringParameters – it’ll be in the event body encoded as a query string
+  // queryStringParameters – it’ll be in the event body encoded as a queryString
   const params = querystring.parse(event.body);
   const name = params.name || "World";
-  console.log(params);
-  return {
-    statusCode: 200,
-    body: `Hello, ${name}`
-  };
+
+  // Send greeting to Slack
+  return fetch(process.env.SLACK_WEBHOOK_URL, {
+    headers: {
+      "content-type": "application/json"
+    },
+    method: "POST",
+    body: JSON.stringify({ text: `${name} says hello!` })
+  })
+    .then(() => ({
+      statusCode: 200,
+      body: `Hello, ${name}! Your greeting has been sent to Slack 👋`
+    }))
+    .catch(error => ({
+      statusCode: 422,
+      body: `Oops! Something went wrong. ${error}`
+    }));
 };
