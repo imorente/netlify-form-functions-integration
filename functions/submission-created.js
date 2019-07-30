9cb0f91b5770caaf64764fee0a6dcde318f85865
Irene Morente <irene@netlify.com>
2019-07-29 20:05:58 -0700

    Send email

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,5 +1,5 @@
 const fetch = require("node-fetch");
-
+const mandrill = require("mandrill-api/mandrill");
 exports.handler = async (event, context) => {
   // Only allow POST
   if (event.httpMethod !== "POST") {
@@ -8,7 +8,36 @@ exports.handler = async (event, context) => {
 
   const { name, email, message } = JSON.parse(event.body).payload || {};
 
-  console.log("name: " + name);
+  console.log("Sending email via mandrill");
+
+  const mandrillClient = new mandrill.Mandrill(process.env.MANDRILL_API_KEY);
+  const emailMessage = {
+    html: "<p>Example HTML content</p>",
+    text: "Example text content",
+    subject: "example subject",
+    from_email: "message.from_email@example.com",
+    from_name: "Example Name",
+    to: [
+      {
+        email: email,
+        name: name,
+        type: "to"
+      }
+    ],
+    headers: {
+      "Reply-To": "message.reply@example.com"
+    },
+    subaccount: "marketing-oreilly-book"
+  };
+  mandrillClient.messages.send(
+    { message: emailMessage },
+    result => {
+      console.log(result);
+    },
+    err => {
+      console.log("A mandrill error occurred: " + e.name + " - " + e.message);
+    }
+  );
 
   // Send greeting to Slack
   return fetch(process.env.SLACK_WEBHOOK_URL, {
@@ -16,7 +45,7 @@ exports.handler = async (event, context) => {
       "content-type": "application/json"
     },
     method: "POST",
-    body: JSON.stringify({ text: `${name} says hello!` })
+    body: JSON.stringify({ text: `${name} says ${message}!` })
   })
     .then(() => ({
       statusCode: 200,
