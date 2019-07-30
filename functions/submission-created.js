5c0aa42acf1329530f8880143c22b1eda4e9cf1f
Irene Morente <irene@netlify.com>
2019-07-29 20:59:45 -0700

    Use node=fetch to send email

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,58 +1,50 @@
 const fetch = require("node-fetch");
-const mandrill = require("mandrill-api/mandrill");
+
 exports.handler = async (event, context) => {
   // Only allow POST
   if (event.httpMethod !== "POST") {
     return { statusCode: 405, body: "Method Not Allowed" };
   }
 
-  const { name, email, message } = JSON.parse(event.body).payload || {};
+  const { name, email } = JSON.parse(event.body).payload || {};
 
   console.log("Sending email via mandrill");
 
-  const mandrillClient = new mandrill.Mandrill(process.env.MANDRILL_API_KEY);
-  const emailMessage = {
-    html: "<p>Example HTML content</p>",
-    text: "Example text content",
-    subject: "example subject",
-    from_email: "message.from_email@example.com",
-    from_name: "Example Name",
-    to: [
-      {
-        email: email,
-        name: name,
-        type: "to"
-      }
-    ],
+  return fetch("https://mandrillapp.com/api/1.0/messages/send.json", {
     headers: {
-      "Reply-To": "message.reply@example.com"
-    },
-    subaccount: "marketing-oreilly-book"
-  };
-  mandrillClient.messages.send(
-    { message: emailMessage },
-    result => {
-      console.log("Email sent:" + result);
-    },
-    err => {
-      console.log("A mandrill error occurred: " + e.name + " - " + e.message);
-    }
-  );
-
-  // Send greeting to Slack
-  return fetch(process.env.SLACK_WEBHOOK_URL, {
-    headers: {
-      "content-type": "application/json"
+      "content-type": "application/json",
+      "X-MC-Subaccount": "marketing-oreilly-book"
     },
     method: "POST",
-    body: JSON.stringify({ text: `${name} says ${message}!` })
+    body: JSON.stringify({
+      key: process.env.MANDRILL_API_KEY,
+      message: {
+        from_email: "irene@netlify.com",
+        to: [
+          {
+            email: email,
+            name: name,
+            type: "to"
+          }
+        ],
+        autotext: "true",
+        subject: "test",
+        html: "test"
+      }
+    })
   })
-    .then(() => ({
-      statusCode: 200,
-      body: `Hello, ${name}! Your greeting has been sent to Slack 👋`
-    }))
-    .catch(error => ({
-      statusCode: 422,
-      body: `Oops! Something went wrong. ${error}`
-    }));
+    .then(() => {
+      console.log("email sent");
+      return {
+        statusCode: 200,
+        body: "Email sent"
+      };
+    })
+    .catch(error => {
+      console.log("Something went wrong: " + error);
+      return {
+        statusCode: 422,
+        body: `Oops! Something went wrong. ${error}`
+      };
+    });
 };
